cbe2094b59ad6c0a3955b8d20355579207b53106
aurelienBOSCH <103254823+aurelienBOSCH@users.noreply.github.com>
2022-06-15 14:36:29 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,13 @@
 import { ActiviteComponent } from './activite/activite.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ActiviteesComponent } from './activitees/activitees.component';
+import { ActivitesComponent } from './activitees/activites.component';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { FicheInscriptionClientComponent } from './fiche-inscription-client/fiche-inscription-client.component';
 import { HorairesComponent } from './horaires/horaires.component';
-
-import { InscriptionAquaBikeComponent } from './inscription-aqua-bike/inscription-aqua-bike.component';
-import { InscriptionAquaGymComponent } from './inscription-aqua-gym/inscription-aqua-gym.component';
-import { InscriptionCoursNatationComponent } from './inscription-cours-natation/inscription-cours-natation.component';
-import { InscriptionPlongeeComponent } from './inscription-plongee/inscription-plongee.component';
-
 import { MenuComponent } from './menu/menu.component';
 import { TableauDeBordComponent } from './tableau-de-bord/tableau-de-bord.component';
-
 import { InscriptionActiviteComponent } from './inscription-activite/inscription-activite.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -24,17 +17,8 @@ import { PlanningComponent } from './planning/planning.component';
 
 
 const routes: Routes = [
-{ path: '',component:TableauDeBordComponent },
-{path:'activites',component:ActiviteesComponent,
-  children:[
-    {path:'aquaGym',component: InscriptionAquaGymComponent},
-    {path:'aquaBike',component:InscriptionAquaBikeComponent},
-    {path:'natation', component:InscriptionCoursNatationComponent},
-    {path:'plongee',component:InscriptionPlongeeComponent}
-
-
-  ]
-},
+{ path: '',component:TableauDeBordComponent},
+{path:'activites',component:ActivitesComponent},
 {path:'activite/:idActivite',component:ActiviteComponent},
 {path:'planning',component:PlanningComponent},
 {path:'contact',component:ContactComponent},
