e27b057d152ce00ec717aff322594c5cc7e0d03e
KhaalAM <au.mollier@gmail.com>
2022-06-15 09:44:22 +0200

    horraires ajoutés

--- src/app/inscription-aqua-bike/inscription-aqua-bike.component.ts
+++ src/app/inscription-aqua-bike/inscription-aqua-bike.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup,Validators,FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-inscription-aqua-bike',
   templateUrl: './inscription-aqua-bike.component.html',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionAquaBikeComponent implements OnInit {
 
-  constructor() { }
+  personneInscri!:FormGroup
+  constructor(private formBuilder:FormBuilder, private router:Router) { }
+
 
   ngOnInit(): void {
+    this.personneInscri = this.formBuilder.group({
+      nom:[null,[Validators.required]],
+      prenom:[null,[Validators.required]],
+      telephone:[null,[Validators.required]],
+      email:[null,[Validators.required]],
+      places:[null,[Validators.required]]
+    })
   }
+
+  onSave(){
+    /*this.XXXXXX.addPersonne(this.personneInscri.value).subscribe()*/
+    this.router.navigate([""])
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
